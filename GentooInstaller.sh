799fa976fa97d8507d267635b2b326bab4a9208d
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-16 22:08:15 +0100

    Update GentooInstaller.sh

--- GentooInstaller.sh
+++ GentooInstaller.sh
@@ -71,12 +71,15 @@ echo "Mounting root partition!"
 mount $root_partition /mnt/gentoo
 cd /mnt/gentoo
 echo "Installing Gentoo with systemd"
-wget http://ftp.rnl.tecnico.ulisboa.pt/pub/gentoo/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/stage3-amd64-desktop-systemd-20230129T164658Z.tar.xz
+wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20230129T164658Z/stage3-amd64-desktop-systemd-20230129T164658Z.tar.xz
 echo "Extracting"
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 nano -w /mnt/gentoo/etc/portage/make.conf
 mkdir /mnt/gentoo/etc/portage/repos.conf
 mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 echo "Changing into target.."
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -86,6 +89,8 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 mount $efi_partition /mnt/gentoo/boot
+chroot "/mnt/gentoo" /bin/bash -c 'source /etc/profile'
+chroot "/mnt/gentoo" /bin/bash -c 'export PS1="(chroot) ${PS1}"'
 chroot "/mnt/gentoo" /usr/bin/emerge --sync
 chroot "/mnt/gentoo" /usr/bin/emerge --sync --quiet
 echo "Showing profiles"
